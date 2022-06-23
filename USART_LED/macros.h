a20aae8ebee172a757f541696eb10a78b2de7638
Emil <emileyvazov1@gmail.com>
2022-06-23 21:01:17 +0200

    Added 7 segment display to display received numbers.

--- USART_LED/macros.h
+++ USART_LED/macros.h
@@ -1,7 +1,7 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define DELAY 1000
+#define DELAY 500
 #define USART_RECEIVE_COMPLETE ((UCSR0A & (1 << RXC0)) != 0)
 
 #endif // MACROS_H
--- USART_LED/main.c
+++ USART_LED/main.c
@@ -1,3 +1,12 @@
+/**
+ * @brief The program will receive/transmit ASCII numbers that will be converted to integers.
+ * In case if USART module didn't receive anything or the received data is not a valid ASCII representation of a number, 
+ *      LEDs will light up in cylon fashion.
+ * When USART module receives data and it is a valid ASCII representation of a number, that number is displayed on 7 segment display.
+ * PORTD[6:7] will be connected to A and B segments of the display correspondingly.
+ * PORTB[0:5] will be connected to [C-G] and DP segments of the display correspondingly.
+ */
+
 #include "macros.h"
 #include "USART.h"
 
@@ -5,7 +14,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED_PORT PORTB
+#define SEGMENT_A 6 // from PORTD
+#define SEGMENT_B 7 // from PORTD
+#define SEGMENT_C 0 // from PORTB
+#define SEGMENT_D 1 // from PORTB
+#define SEGMENT_E 2 // from PORTB
+#define SEGMENT_F 3 // from PORTB
+#define SEGMENT_G 4 // from PORTB
+#define SEGMENT_DP 5 // from PORTB
+
 
 int main(void) {
     uint8_t data = 0, counter_cylon = 0, cylon_forward = 1;
@@ -17,6 +34,9 @@ int main(void) {
     // set pins PB[0:5] to output mode
     DDRB = 0x3F;
 
+    // set pins PD[6:7] to output mode
+    DDRD = 0xC0;
+
     // event loop
     while(1) {
         // receive byte
@@ -25,7 +45,38 @@ int main(void) {
 
             // check if the received number is greater than or equal to '0' ascii character
             if(data - 48 >= 0 && data - 48 <= 9) {
-                LED_PORT = data - 48;
+                if(data - 48 == 0) {
+                    PORTD = (1 << SEGMENT_A) | (1 << SEGMENT_B);
+                    PORTB = (1 << SEGMENT_C) | (1 << SEGMENT_D) | (1 << SEGMENT_E) | (1 << SEGMENT_F);
+                } else if(data - 48 == 1) {
+                    PORTD = (1 << SEGMENT_B);
+                    PORTB = (1 << SEGMENT_C);
+                } else if(data - 48 == 2) {
+                    PORTD = (1 << SEGMENT_A) | (1 << SEGMENT_B);
+                    PORTB = (1 << SEGMENT_D) | (1 << SEGMENT_E) | (1 << SEGMENT_G);
+                } else if(data - 48 == 3) {
+                    PORTD = (1 << SEGMENT_A) | (1 << SEGMENT_B);
+                    PORTB = (1 << SEGMENT_C) | (1 << SEGMENT_D) | (1 << SEGMENT_G);
+                } else if(data - 48 == 4) {
+                    PORTD = (1 << SEGMENT_B);
+                    PORTB = (1 << SEGMENT_C) | (1 << SEGMENT_F) | (1 << SEGMENT_G);
+                } else if(data - 48 == 5) {
+                    PORTD = (1 << SEGMENT_A);
+                    PORTB = (1 << SEGMENT_C) | (1 << SEGMENT_D) | (1 << SEGMENT_F) | (1 << SEGMENT_G);
+                } else if(data - 48 == 6) {
+                    PORTD = (1 << SEGMENT_A);
+                    PORTB = (1 << SEGMENT_C) | (1 << SEGMENT_D) | (1 << SEGMENT_E) | (1 << SEGMENT_F) | (1 << SEGMENT_G);
+                } else if(data - 48 == 7) {
+                    PORTD = (1 << SEGMENT_A) | (1 << SEGMENT_B);
+                    PORTB = (1 << SEGMENT_C);
+                } else if(data - 48 == 8) {
+                    PORTD = (1 << SEGMENT_A) | (1 << SEGMENT_B);
+                    PORTB = (1 << SEGMENT_C) | (1 << SEGMENT_D) | (1 << SEGMENT_E) | (1 << SEGMENT_F) | (1 << SEGMENT_G);
+                } else if(data - 48 == 9) {
+                    PORTD = (1 << SEGMENT_A) | (1 << SEGMENT_B);
+                    PORTB = (1 << SEGMENT_C) | (1 << SEGMENT_F) | (1 << SEGMENT_G);
+                }
+
                 counter_cylon = 0;
                 cylon_forward = 1;
                 flag_invalid_data = 0;
@@ -38,10 +89,16 @@ int main(void) {
             if(counter_cylon == 0)
                 cylon_forward = 1;
             
-            if(counter_cylon >= 5)
+            if(counter_cylon >= 7)
                 cylon_forward = 0;
 
-            LED_PORT = (1 << counter_cylon);
+            if(counter_cylon < 2) {
+                PORTD = (0x40 << counter_cylon);
+                PORTB = 0;
+            } else {
+                PORTD = 0;
+                PORTB = (1 << (counter_cylon - 2));
+            }
             
             if(cylon_forward)
                 ++counter_cylon;
